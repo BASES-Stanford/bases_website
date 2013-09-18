7a86a8d2838900877b26bd7e9b3df67183d695b5
Ray Zhou <rzhou186@stanford.edu>
2013-09-17 19:43:36 -0700

    Added a hash verification function, and worked on fixing scrolling; still bugs with window hashes not being modified

--- public/javascripts/scrollNavigation.js
+++ public/javascripts/scrollNavigation.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-  var destination = window.location.hash;
-  if (destination) scrollTo(destination);
+
+  if (isValidHash(window.location.hash)) {
+    scrollTo(window.location.hash);
+  }
+
+  $("a").click(function(e) {
+    var destination = $(this).attr("href");
+    if (isValidHash(destination)) {
+      e.preventDefault();
+      scrollTo(destination);
+    }
+  });
+
 });
 
 function scrollTo(destination) {
   $("body").animate({
     scrollTop: $(destination).offset().top
     }, "slow");
+}
+
+function isValidHash(hash) {
+  return hash.indexOf("#") === 0;
 }
