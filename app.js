a60032ba0c19f0d91acd7683f15669a9a380228b
Shivaal Roy <shivaal@stanford.edu>
2015-10-03 15:19:20 -0700

    moved staging to actual

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , hackspace = require("./routes/hackspace")
   , http = require('http')
   , path = require('path'),
   rootPath  = path.normalize(__dirname);
@@ -13,8 +12,8 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  // app.set('port', 3000);
-  app.set('port', process.env.PORT || 80);
+  app.set('port', 3000);
+  // app.set('port', process.env.PORT || 80);
   app.use(express.favicon(path.join(__dirname, '/public/favicon.ico')));
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -33,41 +32,30 @@ app.configure('development', function(){
 app.get('/', routes.index);
 
 //Programs
-app.get('/challenge', routes.index);
-app.get('/programs', routes.index);
-app.get('/programs/branding', routes.branding);
+app.get('/programs', routes.programs);
+app.get('/programs/connects', routes.connects);
+app.get('/programs/externalRelations', routes.externalRelations);
+app.get('/programs/socImpact', routes.socImpact);
+app.get('/programs/cnect', routes.cnect);
+app.get('/programs/createspace', routes.createspace);
 app.get('/programs/challenge', routes.challenge);
-app.get('/programs/eChallenge', routes.eChallenge);
-app.get('/programs/socialEChallenge', routes.socialEChallenge);
-app.get('/programs/productShowCase', routes.productShowCase);
-app.get('/programs/etl', routes.etl);
-app.get('/programs/hackspace', routes.hackspace);
-app.get('/programs/hackspace/workshops', hackspace.workshops);
-app.get('/programs/hackspace/workshops/1', hackspace.websiteWorkshop);
-app.get('/programs/profDev', routes.profDev);
-app.get('/programs/core', routes.core);
 app.get('/programs/freshmanBat', routes.freshmanBat);
-app.get('/programs/internship', routes.internship);
-app.get('/programs/WIE', routes.WIE);
+app.get('/programs/careerFairs', routes.careerFairs);
+app.get('/programs/startupLunch', routes.startupLunch);
+app.get('/programs/lessonsInLife', routes.lessonsInLife);
 
-//History
-app.get('/history', routes.index);
-app.get('/history/pastWinners', routes.pastWinners);
-app.get('/history/alumni', routes.alumni);
-app.get('/history/eBootCamp', routes.eBootCamp);
+//Events
+app.get('/events', routes.events);
 
 //Get Involved
-app.get('/getInvolved', routes.index);
-app.get('/getInvolved/careerFairs', routes.careerFairs);
+app.get('/getInvolved', routes.getInvolved);
 app.get('/getInvolved/digest', routes.digest);
 app.get('/getInvolved/registration', routes.registration);
-app.get('/getInvolved/startupLunch', routes.startupLunch);
-app.get('/getInvolved/lessonsInLife', routes.lessonsInLife);
 app.get('/getInvolved/friends', routes.friends);
 
-// Media
+//Media
 app.get('/media', routes.index);
-app.get('/media/events', routes.events);
+app.get('/media/events', routes.mediaevents);
 app.get('/media/inTheNews', routes.inTheNews);
 app.get('/media/brandingGuideLines', routes.brandingGuideLines);
 app.get('/e-bootcamp', routes.eBootCamp);
@@ -77,22 +65,16 @@ app.get('/blog', routes.blog);
 
 //Internal
 //No pages here, just direct link
-app.get('/internal', routes.internal);
+// app.get('/internal', routes.internal);
 
 //Sponsors
 app.get('/sponsors', routes.sponsors);
 
-//E-Guide
-app.get('/eGuide', routes.eGuide);
-app.get('/eGuidePrivate', routes.eGuidePrivate);
-app.get('/eGuideClasses', routes.eGuideClasses);
-app.get('/eGuideGroups', routes.eGuideGroups);
-app.get('/eGuideLabs', routes.eGuideLabs);
-app.get('/eGuideCampusResources', routes.eGuideCampusResources);
-app.get('/eGuideExternalResources', routes.eGuideExternalResources);
-
+//About
 app.get('/about', routes.about);
+app.get('/about/team', routes.team);
 app.get('/about/boardOfAdvisors', routes.boardOfAdvisors);
+app.get('/about/alumni', routes.alumni);
 
 
 
@@ -101,70 +83,70 @@ app.get('/about/boardOfAdvisors', routes.boardOfAdvisors);
 /*************************************
 BEGIN STAGING
 *************************************/
-app.get('/staging/', routes.stagingindex);
-// app.get('/staging/test', routes.stagingtest);
+// app.get('/staging/', routes.stagingindex);
+// // app.get('/staging/test', routes.stagingtest);
 
-//Programs
-app.get('/staging/programs', routes.stagingprograms);
-app.get('/staging/programs/connects', routes.stagingconnects);
-app.get('/staging/programs/externalRelations', routes.stagingexternalRelations);
-app.get('/staging/programs/socImpact', routes.stagingsocImpact);
-app.get('/staging/programs/cnect', routes.stagingcnect);
-app.get('/staging/programs/createspace', routes.stagingcreatespace);
-app.get('/staging/programs/challenge', routes.stagingchallenge);
-app.get('/staging/programs/eChallenge', routes.stagingeChallenge);
-app.get('/staging/programs/socialEChallenge', routes.stagingsocialEChallenge);
-app.get('/staging/programs/productShowCase', routes.stagingproductShowCase);
-app.get('/staging/programs/etl', routes.stagingetl);
-app.get('/staging/programs/hackspace', routes.staginghackspace);
-app.get('/staging/programs/hackspace/workshops', hackspace.workshops);
-app.get('/staging/programs/hackspace/workshops/1', hackspace.websiteWorkshop);
-app.get('/staging/programs/freshmanBat', routes.stagingfreshmanBat);
-app.get('/staging/programs/WIE', routes.stagingWIE);
-app.get('/staging/programs/careerFairs', routes.stagingcareerFairs);
-app.get('/staging/programs/startupLunch', routes.stagingstartupLunch);
-app.get('/staging/programs/lessonsInLife', routes.staginglessonsInLife);
+// //Programs
+// app.get('/staging/programs', routes.stagingprograms);
+// app.get('/staging/programs/connects', routes.stagingconnects);
+// app.get('/staging/programs/externalRelations', routes.stagingexternalRelations);
+// app.get('/staging/programs/socImpact', routes.stagingsocImpact);
+// app.get('/staging/programs/cnect', routes.stagingcnect);
+// app.get('/staging/programs/createspace', routes.stagingcreatespace);
+// app.get('/staging/programs/challenge', routes.stagingchallenge);
+// app.get('/staging/programs/eChallenge', routes.stagingeChallenge);
+// app.get('/staging/programs/socialEChallenge', routes.stagingsocialEChallenge);
+// app.get('/staging/programs/productShowCase', routes.stagingproductShowCase);
+// app.get('/staging/programs/etl', routes.stagingetl);
+// app.get('/staging/programs/hackspace', routes.staginghackspace);
+// app.get('/staging/programs/hackspace/workshops', hackspace.workshops);
+// app.get('/staging/programs/hackspace/workshops/1', hackspace.websiteWorkshop);
+// app.get('/staging/programs/freshmanBat', routes.stagingfreshmanBat);
+// app.get('/staging/programs/WIE', routes.stagingWIE);
+// app.get('/staging/programs/careerFairs', routes.stagingcareerFairs);
+// app.get('/staging/programs/startupLunch', routes.stagingstartupLunch);
+// app.get('/staging/programs/lessonsInLife', routes.staginglessonsInLife);
 
-//Events
-app.get('/staging/events', routes.stagingevents);
+// //Events
+// app.get('/staging/events', routes.stagingevents);
 
-//Get Involved
-app.get('/staging/getInvolved', routes.staginggetInvolved);
-app.get('/staging/getInvolved/digest', routes.stagingdigest);
-app.get('/staging/getInvolved/registration', routes.stagingregistration);
-app.get('/staging/getInvolved/friends', routes.stagingfriends);
+// //Get Involved
+// app.get('/staging/getInvolved', routes.staginggetInvolved);
+// app.get('/staging/getInvolved/digest', routes.stagingdigest);
+// app.get('/staging/getInvolved/registration', routes.stagingregistration);
+// app.get('/staging/getInvolved/friends', routes.stagingfriends);
 
-//Media
-app.get('/staging/media', routes.stagingindex);
-app.get('/staging/media/events', routes.stagingmediaevents);
-app.get('/staging/media/inTheNews', routes.staginginTheNews);
-app.get('/staging/media/brandingGuideLines', routes.stagingbrandingGuideLines);
-app.get('/staging/e-bootcamp', routes.stagingeBootCamp);
+// //Media
+// app.get('/staging/media', routes.stagingindex);
+// app.get('/staging/media/events', routes.stagingmediaevents);
+// app.get('/staging/media/inTheNews', routes.staginginTheNews);
+// app.get('/staging/media/brandingGuideLines', routes.stagingbrandingGuideLines);
+// app.get('/staging/e-bootcamp', routes.stagingeBootCamp);
 
-//Blog
-app.get('/staging/blog', routes.stagingblog);
+// //Blog
+// app.get('/staging/blog', routes.stagingblog);
 
-//Internal
-//No pages here, just direct link
-app.get('/staging/internal', routes.staginginternal);
+// //Internal
+// //No pages here, just direct link
+// app.get('/staging/internal', routes.staginginternal);
 
-//Sponsors
-app.get('/staging/sponsors', routes.stagingsponsors);
+// //Sponsors
+// app.get('/staging/sponsors', routes.stagingsponsors);
 
-//E-Guide
-app.get('/staging/eGuide', routes.stagingeGuide);
-app.get('/staging/eGuidePrivate', routes.stagingeGuidePrivate);
-app.get('/staging/eGuideClasses', routes.stagingeGuideClasses);
-app.get('/staging/eGuideGroups', routes.stagingeGuideGroups);
-app.get('/staging/eGuideLabs', routes.stagingeGuideLabs);
-app.get('/staging/eGuideCampusResources', routes.stagingeGuideCampusResources);
-app.get('/staging/eGuideExternalResources', routes.stagingeGuideExternalResources);
+// //E-Guide
+// app.get('/staging/eGuide', routes.stagingeGuide);
+// app.get('/staging/eGuidePrivate', routes.stagingeGuidePrivate);
+// app.get('/staging/eGuideClasses', routes.stagingeGuideClasses);
+// app.get('/staging/eGuideGroups', routes.stagingeGuideGroups);
+// app.get('/staging/eGuideLabs', routes.stagingeGuideLabs);
+// app.get('/staging/eGuideCampusResources', routes.stagingeGuideCampusResources);
+// app.get('/staging/eGuideExternalResources', routes.stagingeGuideExternalResources);
 
-//About
-app.get('/staging/about', routes.stagingabout);
-app.get('/staging/about/team', routes.stagingteam);
-app.get('/staging/about/boardOfAdvisors', routes.stagingboardOfAdvisors);
-app.get('/staging/about/alumni', routes.stagingalumni);
+// //About
+// app.get('/staging/about', routes.stagingabout);
+// app.get('/staging/about/team', routes.stagingteam);
+// app.get('/staging/about/boardOfAdvisors', routes.stagingboardOfAdvisors);
+// app.get('/staging/about/alumni', routes.stagingalumni);
 /*************************************
 END STAGING
 *************************************/
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
 	res.render('index');
+};
+
+//Programs
+exports.programs = function(req,res) {
+	res.render('programs/index');
+};
+
+exports.connects = function(req,res) {
+	res.render('programs/connects');
+};
+
+exports.externalRelations = function(req,res) {
+	res.render('programs/externalRelations');
+};
+
+exports.socImpact = function(req,res) {
+	res.render('programs/socImpact');
+};
+
+exports.cnect = function(req,res) {
+	res.render('programs/cnect');
+};
+
+exports.createspace = function(req,res) {
+	res.render('programs/createspace');
 };
 
 exports.branding = function(req,res) {
@@ -62,70 +87,63 @@ exports.WIE = function (req, res) {
 	res.render('programs/WIE');
 };
 
-exports.pastWinners = function (req, res) {
-	res.render('history/pastWinners');
-};
-
-exports.alumni = function (req, res) {
-	res.render ('history/alumni');
-};
-
 exports.careerFairs = function (req, res) {
-	res.render ('getInvolved/careerFairs');
+	res.render('programs/careerFairs');
 };
 
 exports.startupLunch = function (req, res) {
-	res.render ('getInvolved/startupLunch');
+	res.render('programs/startupLunch');
 };
 
 exports.lessonsInLife = function(req, res) {
-	res.render('getInvolved/lessonsInLife');
+	res.render('programs/lessonsInLife');
 };
 
+//Events
+exports.events = function(req, res) {
+	res.render('events');
+};
+
+//Get Involved
+exports.getInvolved = function (req, res) {
+	res.render ('getInvolved');
+};
 exports.digest = function (req, res) {
 	res.render ('getInvolved/digest');
 };
-
 exports.registration = function (req, res) {
 	res.render('getInvolved/registration');
 };
-
 exports.friends = function(req, res) {
 	res.render('getInvolved/friends');
-}
+};
 
 exports.blog = function(req, res) {
 	res.redirect('http://stanfordbases.wordpress.com/');
 };
 
-exports.events = function (req, res) {
+//Media
+exports.mediaevents = function (req, res) {
 	res.render('media/events');
 };
-
 exports.inTheNews = function (req, res) {
 	res.render('media/inTheNews');
 };
-
 exports.brandingGuideLines = function (req, res) {
 	res.render('media/brandingGuideLines');
 };
-
 exports.sponsors = function (req, res) {
 	res.render('sponsors');
 };
-
 exports.eGuide = function(req, res) {
 	res.render('eguide/home');
 };
-
 exports.eGuidePrivate = function(req, res) {
 	res.render('eguide/home');
 };
-
 exports.eGuideClasses = function(req, res) {
 	res.render('eguide/classes');
 };
-
 exports.eGuideGroups = function(req, res) {
 	res.render('eguide/groups');
 };
@@ -142,7 +160,12 @@ exports.eGuideExternalResources = function(req, res) {
 	res.render('eguide/externalResources');
 };
 
+//About
 exports.about = function(req, res) {
+	res.render('about/index');
+};
+
+exports.team = function(req, res) {
 	res.render('about/team');
 };
 
@@ -150,185 +173,189 @@ exports.boardOfAdvisors = function (req, res) {
 	res.render('about/boardOfAdvisors');
 };
 
+exports.alumni = function (req, res) {
+	res.render('about/alumni');
+};
+
 /**********************************************************
 STAGING
 **********************************************************/
 
-exports.stagingindex = function(req, res) {
-	res.render('staging/index');
-};
+// exports.stagingindex = function(req, res) {
+// 	res.render('staging/index');
+// };
 
-exports.stagingtest = function(req, res) {
-	res.render('staging/test');
-};
+// exports.stagingtest = function(req, res) {
+// 	res.render('staging/test');
+// };
 
-//Programs
-exports.stagingprograms = function(req,res) {
-	res.render('staging/programs/index');
-};
+// //Programs
+// exports.stagingprograms = function(req,res) {
+// 	res.render('staging/programs/index');
+// };
 
-exports.stagingconnects = function(req,res) {
-	res.render('staging/programs/connects');
-};
+// exports.stagingconnects = function(req,res) {
+// 	res.render('staging/programs/connects');
+// };
 
-exports.stagingexternalRelations = function(req,res) {
-	res.render('staging/programs/externalRelations');
-};
+// exports.stagingexternalRelations = function(req,res) {
+// 	res.render('staging/programs/externalRelations');
+// };
 
-exports.stagingsocImpact = function(req,res) {
-	res.render('staging/programs/socImpact');
-};
+// exports.stagingsocImpact = function(req,res) {
+// 	res.render('staging/programs/socImpact');
+// };
 
-exports.stagingcnect = function(req,res) {
-	res.render('staging/programs/cnect');
-};
+// exports.stagingcnect = function(req,res) {
+// 	res.render('staging/programs/cnect');
+// };
 
-exports.stagingcreatespace = function(req,res) {
-	res.render('staging/programs/createspace');
-};
+// exports.stagingcreatespace = function(req,res) {
+// 	res.render('staging/programs/createspace');
+// };
 
-exports.stagingbranding = function(req,res) {
-	res.render('staging/programs/branding');
-};
+// exports.stagingbranding = function(req,res) {
+// 	res.render('staging/programs/branding');
+// };
 
-exports.stagingchallenge = function(req, res) {
-	res.render('staging/programs/challenge');
-};
+// exports.stagingchallenge = function(req, res) {
+// 	res.render('staging/programs/challenge');
+// };
 
-exports.stagingeChallenge = function(req, res) {
-	res.render('staging/programs/eChallenge');
-};
+// exports.stagingeChallenge = function(req, res) {
+// 	res.render('staging/programs/eChallenge');
+// };
 
-exports.stagingsocialEChallenge = function(req, res) {
-	res.render('staging/programs/socialEChallenge');
-};
+// exports.stagingsocialEChallenge = function(req, res) {
+// 	res.render('staging/programs/socialEChallenge');
+// };
 
-exports.stagingproductShowCase = function(req, res) {
-	res.render('staging/programs/productShowCase');
-};
+// exports.stagingproductShowCase = function(req, res) {
+// 	res.render('staging/programs/productShowCase');
+// };
 
-exports.staginginternal = function(req, res) {
-	res.redirect('https://sites.google.com/site/baseshome/');
-};
+// exports.staginginternal = function(req, res) {
+// 	res.redirect('https://sites.google.com/site/baseshome/');
+// };
 
-exports.stagingeBootCamp = function(req, res) {
-	res.render('staging/history/eBootCamp');
-};
+// exports.stagingeBootCamp = function(req, res) {
+// 	res.render('staging/history/eBootCamp');
+// };
 
-exports.stagingetl = function(req, res) {
-	res.render('staging/programs/etl');
-};
+// exports.stagingetl = function(req, res) {
+// 	res.render('staging/programs/etl');
+// };
 
-exports.staginghackspace = function(req, res) {
-	res.render('staging/programs/hackspace');
-};
+// exports.staginghackspace = function(req, res) {
+// 	res.render('staging/programs/hackspace');
+// };
 
-exports.stagingprofDev = function (req, res) {
-	res.render('staging/programs/profDev');
-};
+// exports.stagingprofDev = function (req, res) {
+// 	res.render('staging/programs/profDev');
+// };
 
-exports.stagingcore = function (req, res) {
-	res.render('staging/programs/core');
-};
+// exports.stagingcore = function (req, res) {
+// 	res.render('staging/programs/core');
+// };
 
-exports.stagingfreshmanBat = function (req, res) {
-	res.render('staging/programs/freshmanBat');
-};
+// exports.stagingfreshmanBat = function (req, res) {
+// 	res.render('staging/programs/freshmanBat');
+// };
 
-exports.staginginternship = function (req, res) {
-	res.render('staging/programs/internship');
-};
+// exports.staginginternship = function (req, res) {
+// 	res.render('staging/programs/internship');
+// };
 
-exports.stagingWIE = function (req, res) {
-	res.render('staging/programs/WIE');
-};
+// exports.stagingWIE = function (req, res) {
+// 	res.render('staging/programs/WIE');
+// };
 
-exports.stagingcareerFairs = function (req, res) {
-	res.render('staging/programs/careerFairs');
-};
+// exports.stagingcareerFairs = function (req, res) {
+// 	res.render('staging/programs/careerFairs');
+// };
 
-exports.stagingstartupLunch = function (req, res) {
-	res.render('staging/programs/startupLunch');
-};
+// exports.stagingstartupLunch = function (req, res) {
+// 	res.render('staging/programs/startupLunch');
+// };
 
-exports.staginglessonsInLife = function(req, res) {
-	res.render('staging/programs/lessonsInLife');
-};
+// exports.staginglessonsInLife = function(req, res) {
+// 	res.render('staging/programs/lessonsInLife');
+// };
 
-//Events
-exports.stagingevents = function(req, res) {
-	res.render('staging/events');
-};
+// //Events
+// exports.stagingevents = function(req, res) {
+// 	res.render('staging/events');
+// };
 
-//Get Involved
-exports.staginggetInvolved = function (req, res) {
-	res.render ('staging/getInvolved');
-};
-exports.stagingdigest = function (req, res) {
-	res.render ('staging/getInvolved/digest');
-};
-exports.stagingregistration = function (req, res) {
-	res.render('staging/getInvolved/registration');
-};
-exports.stagingfriends = function(req, res) {
-	res.render('staging/getInvolved/friends');
-};
+// //Get Involved
+// exports.staginggetInvolved = function (req, res) {
+// 	res.render ('staging/getInvolved');
+// };
+// exports.stagingdigest = function (req, res) {
+// 	res.render ('staging/getInvolved/digest');
+// };
+// exports.stagingregistration = function (req, res) {
+// 	res.render('staging/getInvolved/registration');
+// };
+// exports.stagingfriends = function(req, res) {
+// 	res.render('staging/getInvolved/friends');
+// };
 
-exports.stagingblog = function(req, res) {
-	res.redirect('http://stanfordbases.wordpress.com/');
-};
+// exports.stagingblog = function(req, res) {
+// 	res.redirect('http://stanfordbases.wordpress.com/');
+// };
 
-//Media
-exports.stagingmediaevents = function (req, res) {
-	res.render('staging/media/events');
-};
-exports.staginginTheNews = function (req, res) {
-	res.render('staging/media/inTheNews');
-};
-exports.stagingbrandingGuideLines = function (req, res) {
-	res.render('staging/media/brandingGuideLines');
-};
-exports.stagingsponsors = function (req, res) {
-	res.render('staging/sponsors');
-};
-exports.stagingeGuide = function(req, res) {
-	res.render('staging/eguide/home');
-};
-exports.stagingeGuidePrivate = function(req, res) {
-	res.render('staging/eguide/home');
-};
-exports.stagingeGuideClasses = function(req, res) {
-	res.render('staging/eguide/classes');
-};
-exports.stagingeGuideGroups = function(req, res) {
-	res.render('staging/eguide/groups');
-};
+// //Media
+// exports.stagingmediaevents = function (req, res) {
+// 	res.render('staging/media/events');
+// };
+// exports.staginginTheNews = function (req, res) {
+// 	res.render('staging/media/inTheNews');
+// };
+// exports.stagingbrandingGuideLines = function (req, res) {
+// 	res.render('staging/media/brandingGuideLines');
+// };
+// exports.stagingsponsors = function (req, res) {
+// 	res.render('staging/sponsors');
+// };
+// exports.stagingeGuide = function(req, res) {
+// 	res.render('staging/eguide/home');
+// };
+// exports.stagingeGuidePrivate = function(req, res) {
+// 	res.render('staging/eguide/home');
+// };
+// exports.stagingeGuideClasses = function(req, res) {
+// 	res.render('staging/eguide/classes');
+// };
+// exports.stagingeGuideGroups = function(req, res) {
+// 	res.render('staging/eguide/groups');
+// };
 
-exports.stagingeGuideCampusResources = function(req, res) {
-	res.render('staging/eguide/campusResources');
-};
+// exports.stagingeGuideCampusResources = function(req, res) {
+// 	res.render('staging/eguide/campusResources');
+// };
 
-exports.stagingeGuideLabs = function(req, res) {
-	res.render('staging/eguide/labs');
-};
+// exports.stagingeGuideLabs = function(req, res) {
+// 	res.render('staging/eguide/labs');
+// };
 
-exports.stagingeGuideExternalResources = function(req, res) {
-	res.render('staging/eguide/externalResources');
-};
+// exports.stagingeGuideExternalResources = function(req, res) {
+// 	res.render('staging/eguide/externalResources');
+// };
 
-//About
-exports.stagingabout = function(req, res) {
-	res.render('staging/about/index');
-};
+// //About
+// exports.stagingabout = function(req, res) {
+// 	res.render('staging/about/index');
+// };
 
-exports.stagingteam = function(req, res) {
-	res.render('staging/about/team');
-};
+// exports.stagingteam = function(req, res) {
+// 	res.render('staging/about/team');
+// };
 
-exports.stagingboardOfAdvisors = function (req, res) {
-	res.render('staging/about/boardOfAdvisors');
-};
+// exports.stagingboardOfAdvisors = function (req, res) {
+// 	res.render('staging/about/boardOfAdvisors');
+// };
 
-exports.stagingalumni = function (req, res) {
-	res.render('staging/about/alumni');
-};
+// exports.stagingalumni = function (req, res) {
+// 	res.render('staging/about/alumni');
+// };
